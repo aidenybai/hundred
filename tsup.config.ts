b176137b0755af7302976365a53626c9c2007f06
Aiden Bai <aiden.bai05@gmail.com>
2022-02-24 21:28:47 -0800

    Refactor build pipeline

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  format: ['esm', 'cjs'],
+  minify: true,
+  dts: true,
+});
