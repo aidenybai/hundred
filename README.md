0c1ad60e085ac47c273108ec821fa13a27fa1d4f
Aiden Bai <aiden.bai05@gmail.com>
2023-04-24 20:54:15 -0700

    feat: update to block dom

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # 💯 Hundred <img src="https://badgen.net/badgesize/brotli/https/unpkg.com/hundred?color=000000&labelColor=00000&label=bundle%20size" alt="Code Size" /> <a href="https://www.npmjs.com/package/hundred" target="_blank"><img src="https://img.shields.io/npm/v/hundred?style=flat&colorA=000000&colorB=000000" alt="NPM Version" /></a>
 
-### `<1kb` Hyper-lightweight Virtual DOM
-
-Hundred is intended to be the **most lightweight (`<1kb`) virtual DOM** implementation, and is a proof-of-concept and a learning resource more than a tool you should actually use in production.
-
-Additionally, just because Hundred is lightweight doesn't mean it's performant by any means. It is **faster and smaller** than [`simple-virtual-dom`](https://github.com/livoras/simple-virtual-dom), but ranks behind [production-ready Virtual DOM libraries](https://million.js.org/benchmarks/official-benchmarks).
-
-> If you're looking for something a bit more comprehensive, check out [**Million**](https://github.com/aidenybai/million) — Virtual DOM into the future! 💥🦁✨
->
-> -Aiden ([@aidenybai](https://github.com/aidenybai))
+Hundred is intended to be a toy block virtual DOM based off of [Million.js](https://github.com/aidenybai/million), and is a proof-of-concept and a learning resource more than a tool you should actually use in production.
 
 ## Install Hundred
 
@@ -21,16 +13,20 @@ npm install hundred
 ## Usage
 
 ```js
-import { h, createElement, patch } from 'hundred';
+import { h, block } from 'hundred';
 
-const el = createElement(h('div'));
+const Button = block(({ number }) => {
+  return h('button', null, number);
+});
 
-patch(el, h('div', null, 'Hello World!'), h('div'));
+const button = Button({ number: 0 });
+
+button.mount(document.getElementById('root'));
+
+setInterval(() => {
+  button.patch(Button({ number: Math.random() }));
+}, 100);
 ```
-
-`patch()` function has a standard interface that is used in many Virtual DOM libraries. First argument is a DOM node that will be used as the live DOM reference, and the second one is a Virtual DOM to render.
-
-`h()` function will instantiate a "Virtual DOM" node for an element.
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,158 @@
-type Props = Record<string, unknown>;
+// Standard types for the virtual DOM
+type Props = Record<string, any>;
 interface VElement {
-  tag: string;
+  type: string;
   props: Props;
   children: VNode[];
 }
 type VNode = VElement | string;
 
-export const h = (tag: string, props: Props = {}, children: VNode[] = []): VElement => ({
-  tag,
+export const h = (
+  type: string,
+  props: Props = {},
+  ...children: VNode[]
+): VElement => ({
+  type,
   props,
   children,
 });
 
-export const createElement = (vnode: VNode): HTMLElement | Text => {
+// Edits: these identify "holes" in the virutal dom at specific points.
+// Later, can be used to patch the real dom.
+interface EditAttribute {
+  type: 'attribute';
+  path: number[]; // walk path to the element / hole
+  propName: string;
+  hole: any; // prop key of the hole (e.g. prop.name => "name")
+}
+
+interface EditText {
+  type: 'text';
+  path: number[];
+  index: number;
+  hole: any;
+}
+
+type Edit = EditAttribute | EditText;
+
+// contains the prop key of the hole
+class Hole {
+  key: string;
+  constructor(key: string) {
+    this.key = key;
+  }
+}
+
+// Turns the virtual dom into a real dom, and collects edits along the way.
+// Edits are used to patch the real dom later.
+export const render = (
+  vnode: VNode,
+  edits: Edit[] = [],
+  path: number[] = []
+): HTMLElement | Text => {
   if (typeof vnode === 'string') return document.createTextNode(vnode);
 
-  const el = document.createElement(vnode.tag);
+  const el = document.createElement(vnode.type);
 
-  for (const prop in vnode.props) {
-    el[prop] = vnode.props[prop];
+  if (vnode.props) {
+    for (const prop in vnode.props) {
+      const propValue = vnode.props[prop];
+      if (propValue instanceof Hole) {
+        edits.push({
+          type: 'attribute',
+          path,
+          propName: prop,
+          hole: propValue.key,
+        });
+        continue;
+      }
+      el[prop] = propValue;
+    }
   }
 
-  for (const child of vnode.children) {
-    el.appendChild(createElement(child));
+  for (let i = 0; i < vnode.children?.length; i++) {
+    const child = vnode.children[i];
+    if (child instanceof Hole) {
+      edits.push({
+        type: 'text',
+        path,
+        index: i,
+        hole: child.key,
+      });
+      continue;
+    }
+    // we use the path to keep track of where we are in the tree
+    el.appendChild(render(child, edits, [...path, i]));
   }
 
   return el;
 };
 
-export const patch = (el: HTMLElement | Text, newVNode?: VNode, oldVNode?: VNode): void => {
-  if (!newVNode && newVNode !== '') return el.remove();
-  if (typeof oldVNode === 'string' || typeof newVNode === 'string') {
-    if (oldVNode !== newVNode) return el.replaceWith(createElement(newVNode));
-  } else {
-    if (oldVNode?.tag !== newVNode?.tag) {
-      return el.replaceWith(createElement(newVNode));
-    }
+interface BlockInstance {
+  props: Props;
+  mount: (parent: HTMLElement) => void;
+  patch: (newBlock: BlockInstance) => void;
+}
 
-    for (const prop in { ...oldVNode.props, ...newVNode.props }) {
-      if (newVNode.props[prop] === undefined) {
-        delete el[prop];
-      } else if (
-        oldVNode.props[prop] === undefined ||
-        oldVNode.props[prop] !== newVNode.props[prop]
-      ) {
-        el[prop] = newVNode.props[prop];
+export const block = (fn: (props: Props) => VNode) => {
+  const proxy = new Proxy(
+    {},
+    {
+      get(_, prop: string) {
+        return new Hole(prop);
+      },
+    }
+  );
+  const edits: Edit[] = [];
+  const vnode = fn(proxy);
+  const root = render(vnode, edits);
+
+  const Block = (props: Props): BlockInstance => {
+    const cachedElements = new Map<number, Node>();
+    const mount = (parent: HTMLElement) => {
+      const el = root.cloneNode(true);
+      parent.appendChild(el);
+
+      for (let i = 0; i < edits.length; i++) {
+        const edit = edits[i];
+        // walk the tree to find the element / hole
+        let currentEl = el;
+        for (let i = 0; i < edit.path.length; i++) {
+          currentEl = currentEl.childNodes[edit.path[i]];
+        }
+        cachedElements.set(i, currentEl);
+
+        const value = props[edit.hole];
+
+        if (edit.type === 'attribute') {
+          currentEl[edit.propName] = value;
+        } else if (edit.type === 'text') {
+          const textNode = document.createTextNode(value);
+          currentEl.insertBefore(textNode, currentEl.childNodes[edit.index]);
+        }
       }
-    }
+    };
 
-    for (let i = (oldVNode.children?.length ?? 0) - 1; i >= 0; --i) {
-      patch(
-        <HTMLElement | Text>el.childNodes[i],
-        (newVNode.children || [])[i],
-        oldVNode.children[i],
-      );
-    }
+    const patch = (newBlock: BlockInstance) => {
+      for (let i = 0; i < edits.length; i++) {
+        const edit = edits[i];
+        const value = props[edit.hole];
+        const newValue = newBlock.props[edit.hole];
 
-    for (let i = oldVNode.children?.length ?? 0; i < newVNode.children?.length ?? 0; i++) {
-      el.appendChild(createElement(newVNode.children[i]));
-    }
-  }
+        if (value === newValue) continue;
+
+        const currentEl = cachedElements.get(i)!;
+
+        if (edit.type === 'attribute') {
+          currentEl[edit.propName] = newValue;
+        } else if (edit.type === 'text') {
+          currentEl.childNodes[edit.index].textContent = newValue;
+        }
+      }
+    };
+
+    return { mount, patch, props };
+  };
+
+  return Block;
 };
